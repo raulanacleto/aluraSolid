a33837127058b06fe2b3f14c3b56908abd716bec
Raul Anacleto <raulanacletosilva@gmail.com>
2019-06-19 11:10:19 -0300

    finalizando

--- src/exemplo3/CalculadoraDePrecos.java
+++ src/exemplo3/CalculadoraDePrecos.java
@@ -2,12 +2,18 @@ package exemplo3;
 
 public class CalculadoraDePrecos {
 
+    private TabelaDePreco tabela;
+    private ServicoDeEntrega entrega;
+
+    public CalculadoraDePrecos(TabelaDePreco tabela, ServicoDeEntrega entrega) {
+        this.tabela = tabela;
+        this.entrega = entrega;
+    }
+
     public double calcula(Compra produto) {
-        TabelaDePrecoPadrao tabela = new TabelaDePrecoPadrao();
-        Frete correios = new Frete();
 
         double desconto = tabela.descontoPara(produto.getValor());
-        double frete = correios.para(produto.getCidade());
+        double frete = entrega.para(produto.getCidade());
 
         return produto.getValor() * (1 - desconto) + frete;
     }
